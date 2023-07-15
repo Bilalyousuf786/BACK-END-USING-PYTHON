06a51ad16e18e6088d98da929965ba4fd8e422ea
Bilalyousuf786 <bilaljan838@gmail.com>
2023-07-15 12:53:13 +0500

    python

---
+++ main.py
@@ -0,0 +1,62 @@
+ # concatenation
+
+# x = "hello"
+# y = "world"
+
+# print(x + " " + y)
+
+# string interpolation
+'''
+x = "Hello "
+y = "My world"
+z = f"Hello {x} {y}"
+print(x+y)
+print(z)
+
+
+name = "Bilal"
+age = 25
+print(f"my name is {name}. and I am {age}" ) # All 3 ways are same
+print("my name is {}. I am {}".format(name, age)) # All 3 ways are same
+print("my name is %s I am %s"%(name, age)) # All 3 ways are same
+'''
+
+# escape characater
+# name = "my name is Bilal"
+# age = "I'm 30"
+# print(name +"\n" + age)
+# print(name +"\t" + age)
+
+# input function
+# print(input("Enter name plz \n"))
+# name = input("Age plz")
+# print(name)
+
+# name = input("Name")
+# print(f"Hi {name} Welcome back")
+
+# x = int(input("Enter 1st number"))
+
+# print(x*x)
+# y = int(input("Enter 2nd number"))
+# print(x+y)
+
+
+# math_score = input("Enter Maths Score")
+# eng_score = input("Enter English Score")
+# phy_score = input("Enter Physics Score")
+
+# print(f"Math: {math_score} \n English: {eng_score} \n Physics: {phy_score}")
+
+# txt = "sum" + x+y
+x = int(input("Enter a number"))
+y = int(input("Enter 2nd number"))
+#txt = f"sum: {x+y}"
+z=y
+y=x
+x=z
+print(f"x: {x} and y :{y}")
+
+ 
+
+
